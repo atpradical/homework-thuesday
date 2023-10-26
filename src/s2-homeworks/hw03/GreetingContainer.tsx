88012d37fd1727144052c685bbd09524ff61783a
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-10-26 22:20:32 +0300

    add: HW3 fix2

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -42,7 +42,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
-        setName(e.currentTarget.value.trim())
+        setName(e.currentTarget.value)
 
         if (e.currentTarget.value.length) {
             setError('')
