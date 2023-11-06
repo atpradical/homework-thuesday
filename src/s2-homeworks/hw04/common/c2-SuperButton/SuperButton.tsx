e05f8775514002c877338967860915e5a655437f
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-11-06 19:59:24 +0300

    add: HW4 thuesday

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,10 +18,19 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
+        + (
+            disabled
+                ? ' ' + s.disabled
+                : xType === 'red'
+                    ? ' ' + s.red
+                    : xType === 'secondary'
+                        ? ' ' + s.secondary
+                        : ' ' + s.default
+        )
         // + (disabled
         //         ? ...
         //         : xType === 'red'
-        //             ? ...
+        //             ? ... )
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
